8af9de41ca1038e3fbf4c710f75c62c598de6bcd
Rodrigo Perazzo <rpr@cesar.org.br>
2018-03-17 12:13:24 -0300

    Add Wrapper to observe UseCases on main thread

---
+++ app/src/main/java/br/org/cesar/knot/kegerator/executor/ObservableUseCase.kt
@@ -0,0 +1,34 @@
+package br.org.cesar.knot.kegerator.executor
+
+import io.reactivex.Single
+import io.reactivex.observers.DisposableSingleObserver
+import io.reactivex.schedulers.Schedulers
+import br.org.cesar.knot.kegerator.domain.interactor.UseCase
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.disposables.Disposable
+
+/**
+ * Wrapper class to execute uses cases considering subscribe/observe threads
+ */
+open class ObservableUseCase<Result, in Params> constructor(
+        private val useCase: UseCase<Result, Params>,
+        private val threadExecutor: ThreadExecutor,
+        private val postExecutionThread: PostExecutionThread
+) {
+
+    internal val disposables = CompositeDisposable()
+
+    open fun execute(singleObserver: DisposableSingleObserver<Result>, params: Params? = null) {
+        val single = useCase.execute(params)
+                .subscribeOn(Schedulers.from(threadExecutor))
+                .observeOn(postExecutionThread.scheduler) as Single<Result>
+        addDisposable(single.subscribeWith(singleObserver))
+    }
+
+    fun dispose() {
+        if (!disposables.isDisposed) disposables.dispose()
+    }
+
+    private fun addDisposable(disposable: Disposable) = disposables.add(disposable)
+
+}
